f1ea3554836b785234ef01dd04d6f0cbace7bf80
soulrafael <rafaelbsoul@gmail.com>
2024-07-02 23:15:23 -0300

    first commit

---
+++ src/ConsumerTest.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+//Aceita argumentos do tipo T e não retorna nenhum valor
+
+public class ConsumerTest {
+    public static void main(String[] args) {
+        //Cria uma lista de numeros inteiros
+        List<Integer> lista =  Arrays.asList(1,2,3,4,5,6,7,8);
+
+        Consumer<Integer> numerosPar = numero -> {
+            if (numero%2 == 0) {
+                System.out.println(numero);
+            }
+        };
+        lista.stream().forEach(numerosPar);
+    }
+}
